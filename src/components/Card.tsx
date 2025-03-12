5c23c37b9bbe989c32ba749f2525e66a6024ee0b
Rtxppp <popeangping1815@gmail.com>
2025-03-12 17:22:20 +0700

    final

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,19 @@
 import styles from './card.module.css'
 import Image from 'next/image'
+import InteractiveCard from './InteractiveCard'
 
 
 
 export default function Card({venueName,imgSrc}:{venueName:string,imgSrc:string}){
     return(
-        <div className="w-1/5 h-[300px] rounded-lg shadow-lg bg-white">
-      <div className="w-full h-[70%] relative rounded-t-lg">
-        <Image src={imgSrc} alt={venueName}  objectFit="cover" fill={true} className='object-cover rounded-t-lg' />
-      </div>
-      <div className="p-4 text-center">
-        <h1 className="text-xl font-bold text-gray-900">{venueName}</h1>
-      </div>
-    </div>
+      <InteractiveCard contentName={venueName}>
+      <div className='w-full h-[300px] rounded-lg shadow-lg' >
+          
+          <div className='w-full h-[70%] relative rounded-t-lg'>
+              <Image src={imgSrc} alt='Product Picture' layout="fill"  className='object-cover rounded-t-lg'/>
+          </div>
+              <div className={styles.cardtext}>{venueName}</div>
+          </div>
+      </InteractiveCard>
     )
 }
